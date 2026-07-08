91e8e366e54117f40209b97a6c496b001db04e64
Amit Dubey <amit4528990@gmail.com>
2026-07-08 12:57:12 +0530

    new issue testing

--- config/config/settings.py
+++ config/config/settings.py
@@ -169,7 +169,8 @@ LOGIN_URL= 'accounts:login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
