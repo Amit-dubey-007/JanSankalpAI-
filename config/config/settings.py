0b5df752d32d10e8a3d931efee578202ee37f782
Amit Dubey <amit4528990@gmail.com>
2026-07-08 13:26:56 +0530

    switch email to brevo smtp

--- config/config/settings.py
+++ config/config/settings.py
@@ -166,14 +166,22 @@ LOGIN_REDIRECT_URL = 'complaints:public_dashboard'
 
 LOGIN_URL= 'accounts:login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_SSL = True
+# EMAIL_USE_TLS = False
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 GEMINI_API_KEY = config("GEMINI_API_KEY")
 
